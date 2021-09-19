1d6b3726b4ee14aaf8bb9e8c1e9de6a2b005f0a5
Bonswar-Lilian <90867075+Bonswar-Lilian@users.noreply.github.com>
2021-09-19 15:30:23 +0200

    plus besoin de télécharger + playlist

--- botpythonTPD.py
+++ botpythonTPD.py
@@ -2,57 +2,69 @@ import discord
 from discord.ext import commands
 import youtube_dl
 import os
+from discord.ext import commands
+from discord.ext.commands import bot
 from youtubesearchpython import VideosSearch
+from discord.utils import get
+from discord import FFmpegPCMAudio
+from youtube_dl import YoutubeDL
+from discord.ext import commands
+from discordSuperUtils import MusicManager
+
 connected = False
 
+
+players = {}
+queues = {}
+
+
+def donne_url(url, ctx):
+    x = str(url)
+    if '-play' in x:
+        x = x[6:]
+    if '-p' in x:
+        x = x[3:]
+    if not "youtube" in x:
+        videosSearch = VideosSearch(x, limit=1)
+        url2 = videosSearch.result()['result'][0]['link']
+        return url2
+    if "youtube" in x:
+        return x
+
+
 client = commands.Bot(command_prefix="-")
+MusicManager = MusicManager(client, spotify_support=False)
+
+@MusicManager.event
+async def on_play(ctx, player):
+    await ctx.send(f"Now playing: {player.title}")
 
 
 
 @client.command(name='play', aliases=['p'])
-async def play(ctx, url: str):
+async def play(ctx,):
     global connected
-    song_there = os.path.isfile("song.mp3")
-    try:
-        if song_there:
-            os.remove("song.mp3")
-    except PermissionError:
-        await ctx.send("Wait for the current playing music to end or use the 'stop' command")
-        return
-
-
-    channel = ctx.author.voice.channel
-    voiceChannel = discord.utils.get(ctx.guild.voice_channels, name=str(channel))
+    URL = donne_url(ctx.message.content, ctx)
+    voiceChannel = discord.utils.get(ctx.guild.voice_channels, name=str(ctx.author.voice.channel))
+    YDL_OPTIONS = {'format': 'bestaudio', 'noplaylist': 'True'}
+    FFMPEG_OPTIONS = {
+        'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5', 'options': '-vn'}
     voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
-    if connected==False:
-        await voiceChannel.connect()
-
+    if not voice is None:
+        if not voice.is_connected():
+            voice = await voiceChannel.connect()
+    else:
+        voice = await voiceChannel.connect()
     connected = True
-    voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
+    async with ctx.typing():
+        players = await MusicManager.create_player(URL, ctx.author)
+        await ctx.send(URL)
 
-    if not "youtube" in ctx.message.content:
-        ctx.message.content = ctx.message.content[5:]
-        videosSearch = VideosSearch(ctx.message.content, limit=1)
-        x = videosSearch.result()['result'][0]['link']
-        url = x
-        await ctx.send(x)
-
+    if players:
+        if await MusicManager.queue_add(players=players, ctx=ctx) and not await MusicManager.play(ctx):
+            await ctx.send("Ajouter a la grosse QUEUE")
 
 
-    ydl_opts = {
-        'format': 'bestaudio/best',
-        'postprocessors': [{
-            'key': 'FFmpegExtractAudio',
-            'preferredcodec': 'mp3',
-            'preferredquality': '192',
-        }],
-    }
-    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-        ydl.download([url])
-    for file in os.listdir("./"):
-        if file.endswith(".mp3"):
-            os.rename(file, "song.mp3")
-    voice.play(discord.FFmpegPCMAudio("song.mp3"))
 
 
 @client.command()
@@ -89,6 +101,7 @@ async def skip(ctx):
     voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
     voice.stop()
     await ctx.send("J'avoue la musique pue la merde")
+
 
 
 client.run('token')
